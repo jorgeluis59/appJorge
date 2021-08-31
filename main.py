1744e9c8f61f40422a9dd2ed8635b7817d5df37e
jorgeluis59 <89764796+jorgeluis59@users.noreply.github.com>
2021-08-30 22:04:24 -0500

    Actualización

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask 
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 PORT = 8000
@@ -10,7 +12,23 @@ def not_found(error):
 
 @app.route('/', methods=['GET'])
 def index():
-	return "Hola Mundo"
+	#Entrega final Tarea 1 - Jorge Luis
+	#Aplicación con EndPoint que consulta datos de Yahoo Finance - las cotizaciones
+	# en bolsa de la empresa Ford y Facebook
+
+	link_ford='https://finance.yahoo.com/quote/F/financials?p=F'
+	link_facebook='https://finance.yahoo.com/quote/FB?p=FB&.tsrc=fin-srch'
+	headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+	rford=requests.get(link_ford,headers=headers)
+	rfacebook=requests.get(link_facebook,headers=headers)
+
+
+	soupford=BeautifulSoup(rford.text,'lxml')
+	soupfacebook=BeautifulSoup(rfacebook.text,'lxml')
+	ford=soupford.find('span',class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').text.strip()
+	facebook=soupfacebook.find('span',class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').text.strip()
+	return 'Aplicación by Jorge Luis' + '  ' + 'Ford Motor Company F' + ' ' +ford + '  '+ 'Facebook'+' '+ facebook 
+	
 
 if __name__ == '__main__':
 	app.run(port = PORT,debug = DEBUG)
